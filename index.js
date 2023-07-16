832b9200f886f601ceb01da4c00aea16574b6397
santhiyasanthiya <santhiya30032@gmail.com>
2023-07-16 16:53:50 +0530

    add

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
-// app.use('/', routes);
+app.use('/', routes);
 
 app.get("/", function(req, res){
     res.send({message:"Welcome to Email App"})
